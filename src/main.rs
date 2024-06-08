55484b5727e9ba8c4f1aace402779a670d9a4a55
Haislich <josedlv936@gmail.com>
2024-06-09 00:31:43 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::net::TcpListener;
+
+use zero2prod::run;
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    println!(
+        "Listening at port: {}",
+        listener.local_addr().unwrap().port()
+    );
+    run(listener)?.await
+}
